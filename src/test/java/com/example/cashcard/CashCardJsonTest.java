742060bc29c0ea9a5dbcaa8ebfc693a8c00f3933
jaymgurav <mrithyunjay.gurav@decathlon.com>
2023-07-05 16:11:17 +0530

    Writing failing test

---
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,11 @@
+package com.example.cashcard;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Test
+    public void myFirstTest() {
+        assertThat(2 + 2).isEqualTo(43);
+    }
+}
